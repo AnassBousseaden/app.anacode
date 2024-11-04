28ae6fe8cdab938b0f6526f9280c38161d0761d4
Anass <anassbousseaden@yahoo.fr>
2024-11-04 22:30:18 +0100

    trigger

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	kit: {
 		paths: {
-			base: process.argv.includes('dev') ? '' : "app.anacode"
+			base: process.argv.includes('dev') ? '' : "/app.anacode"
 		},
 		adapter: adapter({
 			pages: 'build',
