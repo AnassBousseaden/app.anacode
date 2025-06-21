79fd707810f029011532c2b9b08c917f00935f02
AnassBousseaden <anassbousseaden@yahoo.fr>
2025-06-21 20:00:29 +0200

    Anacode - fixing the home page redirect (attempt-2)
    * before it would not take into account the base_path

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,8 +1,9 @@
 import { redirect } from '@sveltejs/kit';
+import { base } from '$app/paths';
 
 export const prerender = true;
 export const ssr = false;
 
 export function load() {
-	throw redirect(301, '/problems');
+	throw redirect(301, `${base}/problems`);
 }
